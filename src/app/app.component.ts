325a5e89beb7bdcd258ecd78d5f7615eac8708e7
vugutsa <vugutsamercy84@gmail.com>
2020-11-07 11:51:38 +0300

    give the quotes a constructor so as to display in an array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Quotes';
   quote = 'any quote of your choice';
+
+
+  quotes : string[];
+
+  constructor(){
+    this.quotes = ['Your time is limited', 'If you look at what you have in life', 'Life is what happens'];
+  }
 }
