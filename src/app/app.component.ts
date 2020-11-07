529a9b8fc99d5c04e2db9eeaceaefcc4a9a33351
vugutsa <vugutsamercy84@gmail.com>
2020-11-07 12:05:30 +0300

    import the Quote blueprint class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,14 @@ export class AppComponent {
   title = 'Quotes';
   quote = 'any quote of your choice';
 
-
+  quotes: Quote[] = [
+    {id:1, name:'Watch finding Nemo'},
+    {id:2,name:'Buy Cookies'},
+    {id:3,name:'Get new Phone Case'},
+    {id:4,name:'Get Dog Food'},
+    {id:5,name:'Solve math homework'},
+    {id:6,name:'Plot my world domination plan'},
+  ];
   quotes  : string[];
 
   constructor(){
