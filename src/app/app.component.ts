08a91e96dacac09631ef1c71cb7721bb308e071a
vugutsa <vugutsamercy84@gmail.com>
2020-11-09 11:47:53 +0300

    add the README.md

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,18 +8,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Quotes';
   quote = 'any quote of your choice';
-
-  // quotes: Quote[] = [
-  //   {id: 1, name: 'Your time is limited'},
-  //   {id: 2, name: 'If you look at what you have in life'},
-  //   {id: 3, name: 'Life is what happens'},
-  //   {id: 4, name: 'The way to get started'},
-  //   {id: 5, name: 'If life were predictable it would cease'},
-  // ];
-
-// quotes: string[];
-
-//   constructor(){
-//     this.quotes = ['Your time is limited', 'If you look at what you have in life', 'Life is what happens'];
-//   }
 }
